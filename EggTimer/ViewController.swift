cf8d16498e8f60635233e4a5cce12bcf11f4c382
Xinyi Zhu <juliezhu212@outlook.com>
2021-05-23 17:35:07 -0500

    finish

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,31 +2,58 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Xinyi Zhu on 5/23/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    var player: AVAudioPlayer!
     
-
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    
+    var secondsPassed = 0
+    var totalTime = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
+        timer.invalidate()
+        if player != nil {
+            player.stop()
+        }
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            break
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
+        
+        secondsPassed = 0
+        progressBar.progress = 0.0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print("\(secondsPassed) sec")
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            
+            titleLabel.text = "DONE!"
+            playSound()
         }
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
